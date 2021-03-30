d43e6d708c345f2fa605c32de92589b447c542c8
Zac Alexander <zac@znacreative.com>
2021-03-30 17:53:23 +0000

    Determine average assistance wait time

---
+++ 4_queries/avg_assistance_req_wait.sql
@@ -0,0 +1,15 @@
+/* We need to know the average amount of time that students are waiting for an assistance request.
+This is the duration between an assistance request being created, and it being started.
+
+Calculate the average time it takes to start an assistance request.
+
+Return just a single column here.
+Expected Result:
+
+ average_wait_time 
+-------------------
+ 00:08:46.258793
+(1 row) */
+
+SELECT AVG(assistance_requests.started_at - assistance_requests.created_at) as average_wait_time
+FROM assistance_requests;
