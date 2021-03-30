a77331a538f8ce3b93779180b6e1e06ed0ac794a
Zac Alexander <zac@znacreative.com>
2021-03-30 18:24:12 +0000

    Find assignments with most assistance requests

---
+++ 4_queries/most_confusing_assignments.sql
@@ -0,0 +1,23 @@
+/* We need to know which assignments are causing the most assistance requests.
+
+List each assignment with the total number of assistance requests with it.
+
+Select the assignment's id, day, chapter, name and the total assistances.
+Order by total assistances in order of most to least.
+Expected Result:
+
+ id  |       name       | day | chapter |total_requests 
+-----+------------------+-----+---------+------------------
+ 424 | Ullam cumque     |  51 |      12 |           143
+ 423 | Culpa esse sint  |  51 |      11 |           113
+  45 | Quia quasi       |   5 |       7 |            86
+  50 | A rerum          |   6 |       5 |            83
+ 141 | Illo error dolor |  17 |       9 |            82
+ ...
+(424 rows) */
+
+SELECT assignments.id AS id, name, day, chapter, COUNT(assignments.id) AS total_requests
+FROM assignments
+JOIN assistance_requests ON assignment_id = assignments.id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
