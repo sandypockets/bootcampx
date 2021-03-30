e61615337c28c1e4309290e8d681ab5d0caa2189
Zac Alexander <zac@znacreative.com>
2021-03-30 17:53:00 +0000

    Adjust query to return correct results

--- 4_queries/total_cohort_assistance_duration.sql
+++ 4_queries/total_cohort_assistance_duration.sql
@@ -18,9 +18,9 @@ Expected Result:
  ...
  (11 rows) */
 
-SELECT cohorts.name as cohort, SUM(assistance_requests.started_at - assistance_requests.created_at) as total_duration
-FROM cohorts
-JOIN students ON students.cohort_id = cohorts.id
-JOIN assistance_requests ON assistance_requests.student_id = students.id
+SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+FROM assistance_requests
+JOIN students ON students.id = student_id
+JOIN cohorts on cohorts.id = cohort_id
 GROUP BY cohorts.name
 ORDER BY total_duration;
